7ed2e9f23ccf6c17e3c04ed3e4fdf67ffd5fd1a1
Ibne Nahian <evilprince2009@yahoo.com>
2024-11-29 23:49:03 +0600

    struct

---
+++ DomeTrain/Point.cs
@@ -0,0 +1,18 @@
+namespace DomeTrain;
+
+public struct Point
+{
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public Point Move(int dx, int dy)
+    {
+        return new Point(X + dx, Y + dy);
+    }
+}
