dd7b6a14a8117256880e981a46bb0b2fc3359eb7
Ibne Nahian <evilprince2009@yahoo.com>
2024-11-30 00:34:23 +0600

    messy chaining

--- DomeTrain/Program.cs
+++ DomeTrain/Program.cs
@@ -5,25 +5,29 @@
         static void Main(string[] args)
         {
             List<Product> fruits_bucket = [
-                new("Pineapple", 399), 
-                new("Strawberry", 450), 
-                new("Blueberry", 520), 
+                new("Pineapple", 399),
+                new("Strawberry", 450),
+                new("Blueberry", 520),
                 new("Mango", 475),
-                new("Banana", 410), 
-                new("Avocado", 490), 
-                new("Coffee", 505), 
-                new("Coconut", 430), 
+                new("Banana", 410),
+                new("Avocado", 490),
+                new("Coffee", 505),
+                new("Coconut", 430),
                 new("Grape", 540),
                 new("Almond", 525),
                 new("Apple", 410),
             ];
-           
-           
-            var selected_fruits = fruits_bucket
-                .Where(f => f.Price > 300 && f.Price < 450)
-                .OrderBy(n => n.Price).ToList();
 
-            Parallel.ForEach(selected_fruits, fruit => Console.WriteLine(fruit.Name));
+
+            // var selected_fruits = fruits_bucket
+            //     .Where(f => f.Price >= 300 && f.Price <= 450)
+            //     .OrderBy(n => n.Price).ToList();
+
+            // Parallel.ForEach(selected_fruits, fruit => Console.WriteLine(fruit.Name));
+
+            Parallel.ForEach(fruits_bucket
+                .Where(f => f.Price >= 300 && f.Price <= 450)
+                .OrderBy(n => n.Price).ToList(), fruit => Console.WriteLine(fruit.Name));
         }
     }
 }
