c9baf32038c90e5518ae921320e2e4ba77bf84ea
Ibne Nahian <evilprince2009@yahoo.com>
2024-11-30 00:26:56 +0600

    records with parallel foreach

---
+++ DomeTrain/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace DomeTrain
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> fruits_bucket = [
+                new("Pineapple", 399), 
+                new("Strawberry", 450), 
+                new("Blueberry", 520), 
+                new("Mango", 475),
+                new("Banana", 410), 
+                new("Avocado", 490), 
+                new("Coffee", 505), 
+                new("Coconut", 430), 
+                new("Grape", 540),
+                new("Almond", 525),
+                new("Apple", 410),
+            ];
+           
+           
+            var selected_fruits = fruits_bucket
+                .Where(f => f.Price > 300 && f.Price < 450)
+                .OrderBy(n => n.Price).ToList();
+
+            Parallel.ForEach(selected_fruits, fruit => Console.WriteLine(fruit.Name));
+        }
+    }
+}
