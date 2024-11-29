c4f5e7242c1391c3c70d1aaa00d4bfd62e144cb3
Ibne Nahian <evilprince2009@yahoo.com>
2024-11-30 00:48:39 +0600

    Dummy container

---
+++ DomeTrain/Fruits.cs
@@ -0,0 +1,23 @@
+namespace DomeTrain;
+
+public class Fruits
+{
+    public static readonly List<Product> Bucket =
+    [
+        new("Pineapple", 399),
+        new("Strawberry", 450),
+        new("Blueberry", 520),
+        new("Mango", 475),
+        new("Banana", 410),
+        new("Avocado", 490),
+        new("Coffee", 505),
+        new("Coconut", 430),
+        new("Grape", 540),
+        new("Almond", 525),
+        new("Apple", 410),
+    ];
+    private Fruits()
+    {
+        
+    }
+}
--- DomeTrain/Program.cs
+++ DomeTrain/Program.cs
@@ -4,19 +4,7 @@
     {
         static void Main(string[] args)
         {
-            List<Product> fruits_bucket = [
-                new("Pineapple", 399),
-                new("Strawberry", 450),
-                new("Blueberry", 520),
-                new("Mango", 475),
-                new("Banana", 410),
-                new("Avocado", 490),
-                new("Coffee", 505),
-                new("Coconut", 430),
-                new("Grape", 540),
-                new("Almond", 525),
-                new("Apple", 410),
-            ];
+            var fruits_bucket = Fruits.Bucket;
 
             var fruits_exits = fruits_bucket.Any(f => f.Price < 100);
             Console.WriteLine(fruits_exits);
