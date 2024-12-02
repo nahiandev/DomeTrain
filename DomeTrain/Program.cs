fb2a8ef5f59c746c0344e44a4457da3c31740d2b
Ibne Nahian <evilprince2009@yahoo.com>
2024-12-02 21:29:33 +0600

    #

--- DomeTrain/Program.cs
+++ DomeTrain/Program.cs
@@ -9,6 +9,12 @@ namespace DomeTrain
             var fruits_bucket = Fruits.Instance.Bucket.AsQueryable();
             
             fruits_bucket.Where(f => f.Price <= 200).ToList().ForEach(Console.WriteLine);
+
+
+            var empty = new List<int> { 3, 4,3,6,3,7};
+
+
+            empty.ForEach(Console.WriteLine);
         }
     }
 }
