8d920948f853cd9aa465af56a64326e75d1eb5fe
Ibne Nahian <evilprince2009@yahoo.com>
2024-12-04 15:18:34 +0600

    Action<>, Func<> delegates

--- DomeTrain/ListExtension.cs
+++ DomeTrain/ListExtension.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DomeTrain
+﻿namespace DomeTrain
 {
     internal class ListExtension<T>
     {
--- DomeTrain/Program.cs
+++ DomeTrain/Program.cs
@@ -1,20 +1,27 @@
 ﻿namespace DomeTrain
 {
-    public class Program
+    class Program
     {
-        static async Task Main(string[] args)
-        {
-            List<int> nums = [0,1,2,3,4,5];
+        static void Main()
+        { 
+            List<int> nums = [1, 2, 3, 4, 5, 7, 2, 43, 65, 7, 6, 23, 
+                543, 6, 57, 54, 23, 2, 234, 643];
+            //var result = LambdaOps(500, 600, nums, (n, x, y) => n.Where(a => a > x && a < y)
+            //.ToList());
 
-            List<int> ints = [6, 7, 8, 9, 0, 1, 2, 3, 4, 5];
+            //result.ForEach(Console.WriteLine); 
 
-            var contains = ListExtension<int>.ContainsRange(ints, nums);
+            const int value = 5;
 
-            Console.WriteLine(contains);
+            var flag = ActionOps(nums, value, (n, v) => n.IndexOf(v) is not -1);
 
-            contains = await ListExtension<int>.ContainsRangeAsync(ints, nums);
 
-            Console.WriteLine(contains);
+            Console.WriteLine(flag);
         }
+        private static List<int> LambdaOps(int x, int y, List<int> nums, 
+            Func<List<int>, int, int, List<int>> operation) => operation(nums, x, y);
+
+
+        private static bool ActionOps(List<int> collection, int index, Func<List<int>, int, bool> expression) => expression(collection, index);
     }
 }
