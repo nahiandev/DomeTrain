02dd6a62f99b1cd3ba00709a4010f3c18a4107fe
Ibne Nahian <evilprince2009@yahoo.com>
2024-12-04 20:29:51 +0600

    # returning multiple values with tuple

--- DomeTrain/Program.cs
+++ DomeTrain/Program.cs
@@ -3,25 +3,23 @@
     class Program
     {
         static void Main()
-        { 
-            List<int> nums = [1, 2, 3, 4, 5, 7, 2, 43, 65, 7, 6, 23, 
-                543, 6, 57, 54, 23, 2, 234, 643];
-            //var result = LambdaOps(500, 600, nums, (n, x, y) => n.Where(a => a > x && a < y)
-            //.ToList());
+        {
 
-            //result.ForEach(Console.WriteLine); 
-
-            const int value = 5;
-
-            var flag = ActionOps(nums, value, (n, v) => n.IndexOf(v) is not -1);
+            var VT = (1, 2, 3, 4, 5, "RE", 'E', true, 1.2, 1.3f);
 
 
-            Console.WriteLine(flag);
+            List<int> nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+            var (_, __) = MinMax(nums);
+
+            Console.WriteLine(__);
+           
         }
-        private static List<int> LambdaOps(int x, int y, List<int> nums, 
-            Func<List<int>, int, int, List<int>> operation) => operation(nums, x, y);
+        private static List<int> LambdaOps(int x, int y, List<int> nums, Func<List<int>, int, int, List<int>> operation) => operation(nums, x, y);
+        private static bool ValidateAgainstCondition(List<int> collection, int index, Func<List<int>, int, bool> expression) => expression(collection, index);
 
 
-        private static bool ActionOps(List<int> collection, int index, Func<List<int>, int, bool> expression) => expression(collection, index);
+        private static (int Min, int Max) MinMax(List<int> nums) => (nums.Min(), nums.Max());
+        
     }
 }
