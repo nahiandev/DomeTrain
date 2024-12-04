d4bb44725a4b687fc718d68ab9c54d725eb9f967
Ibne Nahian <evilprince2009@yahoo.com>
2024-12-04 23:48:52 +0600

    # a bit of inheritance

---
+++ DomeTrain/Base.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Channels;
+
+namespace DomeTrain
+{
+    public class Base
+    {
+        // public virtual void Print(string text);
+
+        public virtual void PrintConsole() => Console.WriteLine("Base class method.");
+    }
+
+    public class Instance : Base
+    {
+        public void PrintConsoleBase() => base.PrintConsole();
+
+        // public override void PrintConsole() => base.PrintConsole();
+
+        public override void PrintConsole() => Console.WriteLine("Derived class method.");
+    }
+}
--- DomeTrain/Program.cs
+++ DomeTrain/Program.cs
@@ -1,22 +1,17 @@
-﻿namespace DomeTrain
+﻿using System.IO.Compression;
+
+namespace DomeTrain
 {
     class Program
     {
         static void Main()
         {
+            Instance r = new();
 
-            var VT = (1, 2, 3, 4, 5, "RE", 'E', true, 1.2, 1.3f);
+            r.PrintConsoleBase();
+            r.PrintConsole();
 
-
-            List<int> nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-
-            var (_, max) = MinMax(nums);
-
-            Console.WriteLine(max);
-           
+            Console.WriteLine(r is Base);
         }
-        
-        private static (int Min, int Max) MinMax(List<int> nums) => (nums.Min(), nums.Max());
-        
     }
 }
