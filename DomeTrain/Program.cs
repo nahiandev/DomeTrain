4383a03787604c5c1a5a0c4a8396ab6b3d4c9e8d
Ibne Nahian <evilprince2009@yahoo.com>
2024-12-04 20:32:49 +0600

    # max captured, min discarded

--- DomeTrain/Program.cs
+++ DomeTrain/Program.cs
@@ -10,15 +10,12 @@
 
             List<int> nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
-            var (_, __) = MinMax(nums);
+            var (_, max) = MinMax(nums);
 
-            Console.WriteLine(__);
+            Console.WriteLine(max);
            
         }
-        private static List<int> LambdaOps(int x, int y, List<int> nums, Func<List<int>, int, int, List<int>> operation) => operation(nums, x, y);
-        private static bool ValidateAgainstCondition(List<int> collection, int index, Func<List<int>, int, bool> expression) => expression(collection, index);
-
-
+        
         private static (int Min, int Max) MinMax(List<int> nums) => (nums.Min(), nums.Max());
         
     }
