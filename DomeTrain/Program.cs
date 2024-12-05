c3a8111b293231240158dbf65b093ba3cacf145d
Ibne Nahian <evilprince2009@yahoo.com>
2024-12-05 23:17:28 +0600

    ## various operations using same delegate and different lambda

--- DomeTrain/Program.cs
+++ DomeTrain/Program.cs
@@ -1,16 +1,19 @@
-﻿using System.Text;
-
-namespace DomeTrain
+﻿namespace DomeTrain
 {
     class Program
     {
         static void Main()
-        {
-            DoStuff(7, 5, (x, y) => x * y);
+        { 
+            DoStuff(1, 2, (x, y) => x + y);
+
+            DoStuff(3, 4, (x, y) => x * y);
+
+            DoStuff(7, 6, (x, y) => x * y);
+
+            DoStuff(7, 5, (x, y) => Convert.ToInt32(string.Join("", [x, y]))); 
         }
-
-        private static void DoStuff(int x, int y, Calculate math_ops) => Console.WriteLine(math_ops(x, y));
-
+        
+        private static void DoStuff(int x, int y, Calculate math_ops) => Console.WriteLine(math_ops(x, y)); 
         delegate int Calculate(int x, int y);
     }
 }
