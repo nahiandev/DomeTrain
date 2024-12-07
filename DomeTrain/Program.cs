792a868b47f214024afa58479b3bb1299857fc3a
Ibne Nahian <evilprince2009@yahoo.com>
2024-12-07 14:50:22 +0600

    # self invoking lambda

--- DomeTrain/Program.cs
+++ DomeTrain/Program.cs
@@ -1,36 +1,24 @@
-﻿namespace DomeTrain
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace DomeTrain
 
 {
     class Program
     {
         static void Main()
-        { 
-            Lazy<int> lazy = new(static () =>
-            {
-                const int time = 5;
-                for (int i = 0; i < time; i++)
-                {
-                    Thread.Sleep(1000);
-                    Console.WriteLine($"{5 - i} second(s) to generate a random number.");
-                }
+        {
+            // Equivalent to JS self invoking function
+            // var result = ((x, y) => x + y)(4, 8)
 
+            int result = ((Func<int, int, int>) ((x, y) => x + y))(4, 8);
 
-                return new Random().Next(1, 100);
-            });
-
-
-            Console.WriteLine(lazy.Value);
-            Console.WriteLine(lazy.Value);
-            Console.WriteLine(lazy.Value);
-            Console.WriteLine(lazy.Value);
-            Console.WriteLine(lazy.Value);
-            Console.WriteLine(lazy.Value);
-            Console.WriteLine(lazy.Value);
-            Console.WriteLine(lazy.Value);
+            Console.WriteLine(result);
         }
+
         
-        private static void DoStuff(int x, int y, Calculate math_ops) => Console.WriteLine(math_ops(x, y)); 
         
-        delegate int Calculate(int x, int y);
+        private static void DoStuff(int x, int y, Calculate math_ops) => Console.WriteLine(math_ops(x, y));
+
+        private delegate int Calculate(int x, int y);
     }
 }
