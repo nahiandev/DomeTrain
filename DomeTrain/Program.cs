32c5d66b5b7bdf2ad281f5b1fa43ec8ab66f1225
Ibne Nahian <evilprince2009@yahoo.com>
2024-12-08 16:43:46 +0600

    ##

--- DomeTrain/Program.cs
+++ DomeTrain/Program.cs
@@ -1,28 +1,29 @@
-﻿using System.Security.Cryptography.X509Certificates;
-
-namespace DomeTrain
-
+﻿namespace DomeTrain
 {
     class Program
     {
         static void Main()
         {
-            List<Func<int, int, int>> operations = [Add, Subtract, Multiply, Divide];
+            List<Func<int, int, int>> operations = [Add, Subtract, Multiply, Divide, Concat];
 
             operations.ForEach(action => {
                 Console.WriteLine($"Action name:- {action.Method.Name}, " +
                     $"Result:- {action(12, 10)}");
             });
+
+
+            // DoStuff(9, 5, (x, y) => x * 10 + y);
         }
 
         private static int Multiply(int x, int y) => x * y;
         private static int Add(int x, int y) => x + y;
         private static int Subtract(int x, int y) => x - y;
         private static int Divide(int x, int y) => x / y;
+        private static int Concat(int x, int y) => int.Parse($"{x}{y}");
 
 
 
-        private static void DoStuff(int x, int y, Calculate math_ops) => Console.WriteLine(math_ops(x, y));
+        private static void DoStuff(int x, int y, Calculate expression) => Console.WriteLine(expression(x, y));
 
         private delegate int Calculate(int x, int y);
     }
