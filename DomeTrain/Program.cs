2cd2deea7e094f4a43346236ea263e1f3f43ba7f
Ibne Nahian <evilprince2009@yahoo.com>
2024-12-09 15:03:56 +0600

    xml record traversal

--- DomeTrain/Program.cs
+++ DomeTrain/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Xml;
 
 namespace DomeTrain
 {
@@ -6,9 +7,45 @@ namespace DomeTrain
     {
         static void Main()
         {
-            var text = "Hello World!";
+            //string xml_records = """
+            //    <?xml version="1.0" encoding="UTF-8"?>
+            //    <people>
+            //        <person>
+            //            <name>John Doe</name>
+            //            <age>30</age>
+            //        </person>
+            //        <person>
+            //            <name>Jane Smith</name>
+            //            <age>25</age>
+            //        </person>
+            //        <person>
+            //            <name>Bob Johnson</name>
+            //            <age>40</age>
+            //        </person>
+            //    </people> 
+            // """;
 
-            Console.WriteLine(text.GetUTF8Bytes());
+            string filename = "people.xml";
+
+            
+
+            //File.WriteAllText(filename, xml_records);
+
+            XmlDocument doc = new();
+            doc.Load(filename);
+
+
+            XmlNodeList people = doc.SelectNodes("/people/person")!;
+
+
+            foreach(XmlNode node in people)
+            {
+                Console.WriteLine(node["name"]!.InnerText);
+                Console.WriteLine(node["age"]!.InnerText);
+                Console.WriteLine("---------------------");
+            }
+
+
         }
     }
 }
